ca60768a4bf4f50bce3a05e83708f5558aeb0c20
Paul Craig <paul_craig_16@hotmail.com>
2019-04-03 18:18:32 -0400

    Add repo name to title tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ const document = ({ title, locale, content }) => {
         ${metaIfSHA() || ''}
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>${title}</title>
+        <title>${title} — az-htm</title>
         <style>
           * {
             box-sizing: border-box;
