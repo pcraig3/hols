8164e360a101493350e557f597c657021a8ce2f5
Paul Craig <paul_craig_16@hotmail.com>
2019-04-02 23:40:51 -0400

    Add a name and an address to the dashboard

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -3,13 +3,23 @@ const { css } = require('emotion')
 const Layout = require('../components/Layout.js')
 
 const dashboard = css`
-  code {
+  > div {
+    margin-bottom: 30px;
+  }
+
+  p {
+    padding: 0;
+    margin: 0;
+    margin-bottom: 10px;
+  }
+
+  pre {
+    margin: 0;
     font-size: 1.1em;
     display: inline-block;
     border: 2px solid grey;
     padding: 5px 10px 3px;
     background-color: #f9f9f9;
-    margin-bottom: 20px;
   }
 `
 
@@ -17,13 +27,31 @@ const Dashboard = ({ data: { sin, dobDay, dobMonth, dobYear } = {} }) =>
   html`
     <${Layout}>
       <div class=${dashboard}>
-      <h1>Dashboard</h1>
-      <p>Social Insurance Number</p>
-      <p><code>${sin}</code></p>
-      <p>Date of Birth</p> 
-      <p><code>${`${dobDay}-${dobMonth}-${dobYear}`}</code></p>
-      <br />
-      <a href="/logout">Log out</a>
+        <h1>Dashboard</h1>
+        <div>
+          <p>Name</p>
+          <p><pre>Matthew Morris</pre></p>
+        </div>
+        <div>
+          <p>Social Insurance Number</p>
+          <p><pre>${sin}</pre></p>
+        </div>
+        <div>
+          <p>Date of Birth</p>
+          <p><pre>${`${dobDay}-${dobMonth}-${dobYear}`}</pre></p>
+        </div>
+        <div>
+          <p>Address</p>
+          <p><pre>
+380 Lewis St
+Ottawa
+Ontario
+K2P 2P6
+          </pre></p>
+        </div>
+
+        <br />
+        <a href="/logout">Log out</a>
       </div>
     </${Layout}>
   `
