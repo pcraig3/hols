0a83de37c1a09a722ba8d45a3bec9974e56d7606
Paul Craig <paul_craig_16@hotmail.com>
2019-04-08 00:36:29 -0400

    Use schema for validation errors rather than procedural code
    Schemas are clean, whereas procedural calls get messy.
    Also moved the "array2obj" logic into utils.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const logger = require('morgan')
 const helmet = require('helmet')
 const cookieSession = require('cookie-session')
+const { validationResult, checkSchema } = require('express-validator/check')
 const renderPage = require('./pages/_document.js')
+const { loginSchema, errorArray2ErrorObject } = require('./utils.js')
 
 let locale = 'en'
 const app = express()
@@ -50,53 +52,29 @@ app.get('/login', (req, res) => {
   )
 })
 
-const { body, validationResult } = require('express-validator/check')
+app.post('/login', checkSchema(loginSchema), (req, res) => {
+  // add values to the session, whether or not they validate
+  req.session = getSessionData(req.body)
 
-app.post(
-  '/login',
-  [
-    // name must be at least 2 characters long
-    body('name')
-      .isLength({ min: 2 })
-      .withMessage('must be at least 2 chars long'),
-    // number must be an integer
-    body('number')
-      .isInt({ min: 0 })
-      .withMessage('must be a positive integer'),
-  ],
-  (req, res) => {
-    // add values to the session, whether or not they validate
-    req.session = getSessionData(req.body)
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    const errorObj = errorArray2ErrorObject(errors)
 
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      /*
-      original format is an array of error objects: https://express-validator.github.io/docs/validation-result-api.html 
-      convert that to an object where the key is the parameter name and value is the error object
-      */
-      const newErrors = errors
-        .array({ onlyFirstError: true })
-        .reduce((map, obj) => {
-          map[obj.param] = obj
-          return map
-        }, {})
+    return res.status(422).send(
+      renderPage({
+        locale,
+        pageComponent: 'Login',
+        title: 'Log in',
+        props: {
+          data: getSessionData(req.session),
+          errors: errorObj,
+        },
+      }),
+    )
+  }
 
-      return res.status(422).send(
-        renderPage({
-          locale,
-          pageComponent: 'Login',
-          title: 'Log in',
-          props: {
-            data: getSessionData(req.session),
-            errors: newErrors,
-          },
-        }),
-      )
-    }
-
-    res.redirect(302, '/dashboard')
-  },
-)
+  res.redirect(302, '/dashboard')
+})
 
 app.get('/dashboard', (req, res) => {
   const data = getSessionData(req.session, true)
--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,49 @@ const metaIfSHA = () =>
   process.env.GITHUB_SHA &&
   `<meta name="keywords" content="GITHUB_SHA=${process.env.GITHUB_SHA}" />`
 
+// define a schema for login field validation: https://express-validator.github.io/docs/schema-validation.html
+const loginSchema = {
+  name: {
+    in: ['body'],
+    isEmpty: { errorMessage: 'Cannot be empty', negated: true },
+    isLength: { errorMessage: 'Cannot be 1 character', options: { min: 2 } },
+  },
+  number: {
+    in: ['body'],
+    isEmpty: { errorMessage: 'Cannot be empty', negated: true },
+    isNumeric: { errorMessage: 'Cannot contain letters' },
+    isInt: { errorMessage: 'Cannot be less than 1', options: { min: 1 } },
+  },
+}
+
+/*
+  original format is an array of error objects: https://express-validator.github.io/docs/validation-result-api.html
+  convert that to an object where the key is the parameter name and value is the error object
+
+  ie,
+
+  [
+    { param: 'name', msg: 'Cannot be empty', ... },
+    { param: 'number', msg: 'Cannot be empty', ... }
+  ]
+
+  to
+
+  {
+    name: { param: 'name', msg: 'Cannot be empty', ... },
+    number: { param: 'number', msg: 'Cannot be empty', ... }
+  }
+*/
+const errorArray2ErrorObject = (errors = []) => {
+  return errors.array({ onlyFirstError: true }).reduce((map, obj) => {
+    map[obj.param] = obj
+    return map
+  }, {})
+}
+
 module.exports = {
   html,
   metaIfSHA,
+  loginSchema,
+  errorArray2ErrorObject,
 }
