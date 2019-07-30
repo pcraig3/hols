6ef6265445dcbee5cb25f9861855efa14e3a2eba
Paul Craig <paul_craig_16@hotmail.com>
2019-07-29 20:52:44 -0400

    Escape the github sha value, if it exists

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,13 @@
 const { h } = require('preact')
 const htm = require('htm')
+const validator = require('validator')
 
 const html = htm.bind(h)
 
 // return a meta tag if a GITHUB_SHA environment variable exists
 const metaIfSHA = () =>
   process.env.GITHUB_SHA &&
-  `<meta name="keywords" content="GITHUB_SHA=${process.env.GITHUB_SHA}" />`
+  `<meta name="keywords" content="GITHUB_SHA=${validator.escape(process.env.GITHUB_SHA)}" />`
 
 module.exports = {
   html,
