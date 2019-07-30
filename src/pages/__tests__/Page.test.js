8b86e8ea241293b5a811f0cfbf64e0944caa4bf8
Paul Craig <paul_craig_16@hotmail.com>
2019-07-30 00:01:44 -0400

    Add test for Provinces page
    Also fix tests for the Page page that were expecting a locale.

--- src/pages/__tests__/Page.test.js
+++ src/pages/__tests__/Page.test.js
@@ -4,25 +4,25 @@ const { html } = require('../../utils.js')
 
 const Page = require('../Page.js')
 
-const renderPage = ({ name, locale }) => {
+const renderPage = ({ name }) => {
   return cheerio.load(
     render(
       html`
-        <${Page} ...${{ name, locale }} />
+        <${Page} ...${{ name }} />
       `,
     ),
   )
 }
 
 test('Page component has the name passed into it', () => {
-  const $ = renderPage({ name: 'test', locale: 'en' })
+  const $ = renderPage({ name: 'test' })
   expect($('h1').length).toBe(1)
   expect($('h1').text()).toEqual('test!!')
   expect($('p').text()).toEqual('This page is all about “test”.')
 })
 
 test('Page component accepts no name', () => {
-  const $ = renderPage({ locale: 'en' })
+  const $ = renderPage({})
   expect($('h1').length).toBe(1)
   expect($('h1').text()).toEqual('undefined!!')
   expect($('p').text()).toEqual('This page is all about “”.')
---
+++ src/pages/__tests__/Provinces.test.js
@@ -0,0 +1,40 @@
+const render = require('preact-render-to-string')
+const cheerio = require('cheerio')
+const { html } = require('../../utils.js')
+
+const Provinces = require('../Provinces.js')
+
+const renderPage = ({ provinces }) => {
+  return cheerio.load(
+    render(
+      html`
+        <${Provinces} ...${{ data: { provinces } }} />
+      `,
+    ),
+  )
+}
+
+describe('Provinces component', () => {
+  test('has one province passed into it', () => {
+    const $ = renderPage({ provinces: [{ name_en: 'Aurora' }] })
+    expect($('h1').length).toBe(1)
+    expect($('h1').text()).toEqual('Canadian provinces and territories')
+    expect($('li').length).toBe(1)
+    expect($('li').text()).toBe('Aurora')
+  })
+
+  test('has no provinces passed into it', () => {
+    const $ = renderPage({ provinces: [] })
+    expect($('h1').length).toBe(1)
+    expect($('li').length).toBe(0)
+  })
+
+  test('has several provinces passed into it', () => {
+    const $ = renderPage({
+      provinces: [{ name_en: 'Aurora' }, { name_en: 'Alaska' }, { name_en: 'French Labrador' }],
+    })
+    expect($('h1').length).toBe(1)
+    expect($('li').length).toBe(3)
+    expect($('li').text()).toBe('AuroraAlaskaFrench Labrador')
+  })
+})
