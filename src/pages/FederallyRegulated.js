7a0c4d5ce8786cbfc79c315c990dea870aa3a2e7
Paul Craig <paul_craig_16@hotmail.com>
2019-09-12 02:06:46 -0400

    Make the federally-regulated page a bit readier

--- src/pages/FederallyRegulated.js
+++ src/pages/FederallyRegulated.js
@@ -10,22 +10,27 @@ const FederallyRegulated = () =>
           <h1>Do federal holidays apply to me?</h1>
 
           <p>
-            Probably not. The vast majority of Canadian workers get provincial holidays off, not
-            federal ones.
+            Probably not. Most Canadian workers will get provincial holidays off, not federal ones.
           </p>
-          <p>
-            If you’d like to know more, continue reading below. Otherwise, ${' '}
-            <a href="/provinces">pick your province or territory</a>${' '}to see your next holiday.
-          </p>
+          <aside>
+            <ul>
+              <li>
+                <a href="/provinces">Pick your province or territory</a> to see your next holiday
+              </li>
+              <li>See <a href="/federal">upcoming federal holidays</a></li>
+            </ul>
+          </aside>
 
-          <h2>Who’s federally-regulated?</h2>
+          <h2>Who’s federally regulated?</h2>
           <p>
-            All jobs in Canada are regulated either by a <strong>provincial</strong> or the${' '}
-            <strong>federal</strong> government. Depending on where you work, you will either get
-            provincial holidays or federal holidays.
+            All jobs in Canada are regulated by <strong>the province</strong> or the${' '}<strong
+              >federal</strong
+            >
+            ${' '}government. Depending on where you work, you will either get provincial holidays
+            or federal holidays.
           </p>
           <p>
-            There are many federally-regulated industries. Some examples are:
+            Approximately 6% of Canadians work in federally regulated industries. Some examples are:
           </p>
           <ul>
             <li>banks</li>
@@ -45,8 +50,8 @@ const FederallyRegulated = () =>
 
           <h2>What’s the difference?</h2>
           <p>
-            Sometimes, provincial holidays are the same as federal holidays — like Christmas Day, or
-            New Years — but not always.
+            Sometimes, provincial holidays are the same as federal holidays — like Christmas, or New
+            Year’s — but not always.
           </p>
           <p>
             For example, the province of Ontario doesn’t observe Remembrance Day, which means
@@ -67,11 +72,11 @@ const FederallyRegulated = () =>
           <h2>How can I tell which holidays apply to me?</h2>
           <p>
             Most likely you follow provincial holidays, but if you’re not sure, you can ask someone
-            you work with or check${' '}
+            you work with or check out${' '}
             <a
               href="https://www.canada.ca/en/employment-social-development/programs/employment-equity/regulated-industries.html"
               target="_blank"
-              >the list of federally-regulated industries</a
+              >the list of federally regulated industries</a
             >.
           </p>
 
