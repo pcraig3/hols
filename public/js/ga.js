5e8e273407a2c8593d07272f9b575d593e544752
Paul Craig <paul_craig_16@hotmail.com>
2019-09-22 21:04:19 -0400

    remove console log on each event
    Was just there to verify it was really going to work.
    Now that it works, we can get rid of it.

--- public/js/ga.js
+++ public/js/ga.js
@@ -7,10 +7,10 @@ window.ga = window.ga || function () {
 ga.l = +new Date();
 ga('create', 'UA-37633400-10', 'auto');
 ga('send', 'pageview');
+
 document.addEventListener('click', function (event) {
   if (event.target.dataset.event) {
-    console.log("event: ".concat(event.target.dataset.label)); // ga(‘send’, ‘event’, ‘Category’, ‘Action’, ‘Label’, ‘Value’);
-
+    // ga(‘send’, ‘event’, ‘Category’, ‘Action’, ‘Label’, ‘Value’);
     ga('send', 'event', 'Event', 'Click', event.target.dataset.label);
   }
 }, false);
