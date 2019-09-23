6c3f0f0d45454dbe1eb0366c8a835a03f8effbd5
Paul Craig <paul_craig_16@hotmail.com>
2019-09-22 21:00:38 -0400

    run ga.js through a babel transpilation
    Make sure it works in IE.

--- public/js/ga.js
+++ public/js/ga.js
@@ -1,19 +1,16 @@
 /* eslint-disable */
-window.ga =
-  window.ga ||
-  function() {
-    ;(ga.q = ga.q || []).push(arguments)
-  }
-ga.l = +new Date()
-ga('create', 'UA-37633400-10', 'auto')
-ga('send', 'pageview')
+window.ga = window.ga || function () {
+  ;
+  (ga.q = ga.q || []).push(arguments);
+};
 
+ga.l = +new Date();
+ga('create', 'UA-37633400-10', 'auto');
+ga('send', 'pageview');
 document.addEventListener('click', function (event) {
+  if (event.target.dataset.event) {
+    console.log("event: ".concat(event.target.dataset.label)); // ga(‘send’, ‘event’, ‘Category’, ‘Action’, ‘Label’, ‘Value’);
 
-  if(event.target.dataset.event) {
-    console.log(`event: ${event.target.dataset.label}`)
-    // ga(‘send’, ‘event’, ‘Category’, ‘Action’, ‘Label’, ‘Value’);
-    ga('send', 'event', 'Event', 'Click', event.target.dataset.label)
+    ga('send', 'event', 'Event', 'Click', event.target.dataset.label);
   }
-
 }, false);
