214940c30ed12143a1430c7442a1329b842d2332
Paul Craig <paul_craig_16@hotmail.com>
2019-12-26 21:58:47 -0500

    hide repeat dates on mobile

--- src/components/SummaryTable.js
+++ src/components/SummaryTable.js
@@ -1,10 +1,28 @@
 const { html } = require('../utils')
 const { css } = require('emotion')
-const { theme } = require('../styles')
+const { theme, visuallyHidden } = require('../styles')
 
 const summaryRow = css`
-  margin-bottom: ${theme.space.md};
-  border-bottom: 2px solid ${theme.color.greyLight};
+  padding-top: ${theme.space.md};
+  border-top: 2px solid ${theme.color.greyLight};
+
+  &:first-of-type {
+    border-top: none;
+    padding-top: 0;
+  }
+
+  &.repeatDate {
+    border-top: none;
+    padding-top: ${theme.space.xs};
+
+    .key {
+      ${visuallyHidden};
+    }
+
+    @media (${theme.mq.md}) {
+      padding-top: ${theme.space.md};
+    }
+  }
 
   @media (${theme.mq.lg}) {
     display: table-row;
@@ -26,19 +44,14 @@ const summaryRow = css`
 
   .value {
     white-space: pre-wrap;
-    margin-bottom: ${theme.space.xxs};
-  }
-
-  .value2 {
-    margin: 0;
     margin-bottom: ${theme.space.md};
-
-    a {
-      color: inherit;
-    }
   }
 
   @media (${theme.mq.lg}) {
+    &.repeatDate .key {
+      visibility: hidden;
+    }
+
     &:first-of-type {
       .key,
       .value,
@@ -61,21 +74,30 @@ const summaryRow = css`
       }
     }
 
-    &.repeatDate .key {
-      visibility: hidden;
-    }
-
     .key {
-      width: 60%;
+      width: 70%;
     }
 
     .value {
-      width: 25%;
+      width: 30%;
     }
   }
 `
 
 const summaryRow2 = css`
+  .value {
+    margin-bottom: ${theme.space.xxs};
+  }
+
+  .value2 {
+    margin: 0;
+    margin-bottom: ${theme.space.md};
+
+    a {
+      color: inherit;
+    }
+  }
+
   @media (${theme.mq.lg}) {
     .key {
       width: 55%;
