57f264a1aa288f69427fba1e0236f5ce0e9df2b3
Paul Craig <paul_craig_16@hotmail.com>
2020-01-01 19:02:06 -0500

    separate the links so that we can have two different top menus

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,39 +14,71 @@ const styles = css`
     flex: auto;
     text-align: right;
     font-size: 0.76em;
-  }
 
-  ul {
-    list-style: none;
-    margin: 0;
-    margin-top: ${theme.space.xs};
+    .links--sm {
+      display: initial;
+    }
 
-    a {
-      color: ${theme.color.grey};
+    .links--md {
+      display: none;
     }
 
     @media (${theme.mq.md}) {
-      li {
-        display: inline-block;
-        margin-left: ${theme.space.md};
+      .links--sm {
+        display: none;
+      }
+
+      .links--md {
+        display: initial;
       }
     }
   }
 `
+
+const linkStyles = css`
+  list-style: none;
+  margin: 0;
+
+  a {
+    color: ${theme.color.grey};
+  }
+
+  @media (${theme.mq.md}) {
+    li {
+      display: inline-block;
+      margin-left: ${theme.space.md};
+    }
+  }
+
+  details & {
+    margin-top: ${theme.space.xs};
+  }
+`
+
+const NavLinks = () => {
+  return html`
+    <ul class=${linkStyles}>
+      <li><a href="/federal">Federal holidays</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="#">Feedback</a></li>
+    </ul>
+  `
+}
 
 const Nav = ({ color }) => {
   return html`
     <nav class=${styles}>
       <${Logo} color=${color} />
       <div class="links">
-        <details open>
-          <summary>Menu</summary>
-          <ul>
-            <li><a href="#">Federal holidays</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Feedback</a></li>
-          </ul>
-        </details>
+        <div class="links--sm">
+          <details>
+            <summary>Menu</summary>
+            <${NavLinks} />
+          </details>
+        </div>
+        <div class="links--md">
+          <${NavLinks} />
+        </div>
       </div>
     </nav>
   `
