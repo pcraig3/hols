5e29320edf917d6cb28dcbd22f875e9d965986a5
Paul Craig <paul_craig_16@hotmail.com>
2020-01-01 20:14:32 -0500

    add tests for Nav component
    updated some of my other failing tests as well

--- src/pages/__tests__/Provinces.test.js
+++ src/pages/__tests__/Provinces.test.js
@@ -17,24 +17,24 @@ const renderPage = ({ provinces }) => {
 describe('Provinces component', () => {
   test('has one province passed into it', () => {
     const $ = renderPage({ provinces: [{ nameEn: 'Aurora' }] })
-    expect($('h1').length).toBe(1)
-    expect($('h1').text()).toEqual('All regions in Canada')
-    expect($('li').length).toBe(1)
-    expect($('li').text()).toBe('Aurora')
+    expect($('main h1').length).toBe(1)
+    expect($('main h1').text()).toEqual('All regions in Canada')
+    expect($('main li').length).toBe(1)
+    expect($('main li').text()).toBe('Aurora')
   })
 
   test('has no provinces passed into it', () => {
     const $ = renderPage({ provinces: [] })
-    expect($('h1').length).toBe(1)
-    expect($('li').length).toBe(0)
+    expect($('main h1').length).toBe(1)
+    expect($('main li').length).toBe(0)
   })
 
   test('has several provinces passed into it', () => {
     const $ = renderPage({
       provinces: [{ nameEn: 'Aurora' }, { nameEn: 'Alaska' }, { nameEn: 'French Labrador' }],
     })
-    expect($('h1').length).toBe(1)
-    expect($('li').length).toBe(3)
-    expect($('li').text()).toBe('AuroraAlaskaFrench Labrador')
+    expect($('main h1').length).toBe(1)
+    expect($('main li').length).toBe(3)
+    expect($('main li').text()).toBe('AuroraAlaskaFrench Labrador')
   })
 })
