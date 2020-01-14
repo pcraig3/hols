9eda87e4f3045c4fceb0d2145b2c23ab5d6b8bf5
Paul Craig <paul_craig_16@hotmail.com>
2020-01-14 00:03:40 -0500

    update the text on the about page
    now there's an API page, take away the reference to it.
    also mention that it's on github.

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,14 +7,20 @@ const About = ({ data: { nextHoliday } }) =>
     <${Layout} route="/about">
       <${Content}>
         <h1>About</h1>
-
         <p>
           Work sucks, I know. But until we get the post-work future we all deserve,${' '}
           <a href="/">${nextHoliday.nameEn}</a> is what we have to look forward to.
         </p>
 
-        <h2>API</h2>
-        <p>Yes,${' '}<a href="/api" target="_blank">there’s an API</a>.</p>
+        <h2>“I want to add all the holidays to my calendar”</h2>
+        <p>
+          Heck yes you do! And${' '}
+          <a
+            href="/add-holidays-to-calendar"
+            title="you can add Canada’s 2020 holidays to your calendar"
+            >you can</a
+          >!
+        </p>
 
         <h2>“Something is wrong”</h2>
         <p>Leave me some <a href="/feedback">/feedback</a> and I’ll do my best.</p>
@@ -23,7 +29,9 @@ const About = ({ data: { nextHoliday } }) =>
         <p>
           Hello, my name is${' '}
           <a class="pcraig3" href="https://pcraig3.ca" title="Paul Craig" target="_blank">Paul</a
-          >${' '} and I am paying for this site for some reason.
+          >${' '} and I am paying for this site for some reason.${' '}
+          <a href="https://github.com/pcraig3/hols" target="_blank">Code is on GitHub</a> if you
+          want to “borrow” all my intellectual property.
         </p>
       <//>
     <//>
