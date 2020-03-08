57586a88072b629f47a07947abe23aae0c6df36f
Paul Craig <paul_craig_16@hotmail.com>
2020-03-07 21:00:50 -0500

    Add a little js shim to link buttons
    Pressing the space bar should click a button. Since we are shimming
    links as buttons, we should make sure you can click the space
    bar on them.

--- public/js/script.js
+++ public/js/script.js
@@ -23,3 +23,15 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 }, false);
+
+// Find all of the links with the 'button' role and add a click event to them
+var elements = document.querySelectorAll('a[role="button"]');
+
+for (var i = 0, len = elements.length; i < len; i++) {
+  elements[i].addEventListener('keydown', function (e) {
+    if (e.keyCode == 32) {
+      e.target.click();
+      e.preventDefault();
+    }
+  });
+}
