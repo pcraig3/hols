f9d4e2458488fd484194bddfedcb2fe70a7438a7
Paul Craig <paul_craig_16@hotmail.com>
2020-04-28 23:21:44 -0400

    update the API page
    now that I'm pushing more people to it, clean it up a bit.
    
    - more closely aligns the /api page with the API.md file in the repo
    - mention the OpenAPI spec since that's a big-ish deal

--- src/pages/API.js
+++ src/pages/API.js
@@ -1,34 +1,57 @@
 const { html } = require('../utils')
+const { css } = require('emotion')
 const Layout = require('../components/Layout.js')
 const Content = require('../components/Content.js')
 
-const Feedback = () =>
+const linkStyle = css`
+  text-decoration: none;
+`
+
+const API = () =>
   html`
     <${Layout} route="/api">
       <${Content}>
         <h1>Canada Holidays API</h1>
         <p>
-          <a href="/">canada-holidays.ca</a> is powered by a JSON API that returns Canada’s
-          statutory holidays, and you can use it too.${' '}
+          The ${' '}<a href="https://canada-holidays.ca/api/v1/" target="_blank"
+            >Canada Holidays API</a
+          >${' '}lists all 28 public holidays for all 13 provinces and territories in Canada,
+          including federal holidays.
         </p>
         <p>
           <span aria-hidden="true">👉</span>${' '}
-          <a href="https://canada-holidays.ca/api/v1/" target="_blank"
-            >https://canada-holidays.ca/api/v1/</a
+          <strong
+            ><a class=${linkStyle} href="https://canada-holidays.ca/api/v1/" target="_blank"
+              >https://canada-holidays.ca/api/v1/</a
+            ></strong
           >
-          ${' '}<span aria-hidden="true">👈</span>
+          ${' '}
         </p>
         <p>Features:</p>
         <ul>
           <li>Return holidays with associated regions</li>
           <li>Return regions with associated holidays</li>
-          <li>Filter by federal holidays</li>
-          <li>Filter by national holidays</li>
-          <li>Free forever</li>
-          <li>It’s pretty good</li>
+          <li>Returns federal holidays</li>
+          <li>Returns national holidays</li>
+          <li>Returns “next” holiday for each region</li>
+          <li>
+            Returns holidays for multiple years: <code>2018</code>, <code>2019</code>,${' '}
+            <code>2020</code>, <code>2021</code>, <code>2022</code>.
+          </li>
+        </ul>
+        <p>
+          Plus(!!!) check out all these goodies you get for${' '}
+          <span aria-hidden="true">✨</span> free <span aria-hidden="true">✨</span>:
+        </p>
+
+        <ul>
+          <li>It’s free (<span aria-hidden="true">✨</span>)</li>
+          <li>
+            <a href="https://twitter.com/pcraig3" target="_blank">Dedicated support channel</a>
+          </li>
           <li>Kind of bilingual (EN & FR)</li>
           <li>
-            Nearly compliant with the${' '}
+            Pretty much compliant with the${' '}
             <a
               href="https://www.canada.ca/en/government/system/digital-government/modern-emerging-technologies/government-canada-standards-apis.html"
               target="_blank"
@@ -39,17 +62,36 @@ const Feedback = () =>
             <a href="https://github.com/pcraig3/hols" target="_blank">Open source</a> which is cool
             if you’re a nerd
           </li>
+          <li>
+            <a
+              href="https://github.com/pcraig3/hols/blob/master/reference/Canada-Holidays-API.v1.yaml"
+              target="_blank"
+              >Documented with an OpenAPI spec</a
+            >${' '}which is <em>even more</em> cool for <em>even nerdier</em> nerds
+          </li>
         </ul>
         <p>Definitely use it for your billions of dollars mission-critical system.</p>
 
         <h2>Documentation</h2>
         <p>
-          <a
+          There's an OpenAPI spec at${' '}<a
+            href="https://github.com/pcraig3/hols/blob/master/reference/Canada-Holidays-API.v1.yaml"
+            target="_blank"
+            ><code>Canada-Holidays-API.v1.yaml</code></a
+          >${' '}and a${' '}<a
+            href="https://app.swaggerhub.com/apis/pcraig3/canada-holidays/"
+            target="_blank"
+            >SwaggerHub</a
+          >${' '}page where you can test the endpoints.
+        </p>
+        <p>
+          Otherwise, the${' '}<a
             title="API documentation"
             href="https://github.com/pcraig3/hols/blob/master/API.md"
             target="_blank"
-            >Documentation is here</a
-          >; it’s pretty good. (see point 5 above.)
+            >documentation on GitHub</a
+          >
+          ${' '}is pretty good.
         </p>
 
         <h2>Citations</h2>
@@ -57,7 +99,7 @@ const Feedback = () =>
           Scraped the data from the Wikipedia article “<a
             href="https://en.wikipedia.org/wiki/Public_holidays_in_Canada"
             >Public holidays in Canada</a
-          >” that knows about which provinces observe which holidays and vice-versa. And${' '}
+          >” that tables out which provinces observe which holidays and vice-versa. And${' '}
           <em>then</em>${' '}I even${' '}
           <a href="https://github.com/pcraig3/hols/#citations" title="Citations" target="_blank"
             >double-checked</a
@@ -68,4 +110,4 @@ const Feedback = () =>
     <//>
   `
 
-module.exports = Feedback
+module.exports = API
