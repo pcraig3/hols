1e3d0db41dddc3320dfc1501171c52c90f6981a7
Paul Craig <paul_craig_16@hotmail.com>
2020-05-06 17:33:54 -0400

    add fonts locally
    reduce request times and number of requests.
    
    found a good article and then a good tool to do this:
    - https://johnfraney.ca/posts/2019/12/09/improve-page-speed-google-fonts/
    - https://google-webfonts-helper.herokuapp.com/fonts/gothic-a1?subsets=latin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,7 +32,80 @@ const document = ({ title, content, docProps: { meta, path } }) => {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
-        <link href="https://fonts.googleapis.com/css?family=Gothic+A1:300,400,500,600,700&display=swap" rel="stylesheet" />
+        <!-- link href="https://fonts.googleapis.com/css?family=Gothic+A1:300,400,500,600,700&display=swap" rel="stylesheet" /-->
+        <style>
+
+        /* gothic-a1-300 - latin */
+        @font-face {
+          font-family: 'Gothic A1';
+          font-style: normal;
+          font-weight: 300;
+          font-display: swap;
+          src: url('/fonts/gothic-a1-v8-latin-300.eot'); /* IE9 Compat Modes */
+          src: local('Gothic A1 Light'), local('GothicA1-Light'),
+               url('/fonts/gothic-a1-v8-latin-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+               url('/fonts/gothic-a1-v8-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-300.woff') format('woff'), /* Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-300.ttf') format('truetype'), /* Safari, Android, iOS */
+               url('/fonts/gothic-a1-v8-latin-300.svg#GothicA1') format('svg'); /* Legacy iOS */
+        }
+        /* gothic-a1-regular - latin */
+        @font-face {
+          font-family: 'Gothic A1';
+          font-style: normal;
+          font-weight: 400;
+          font-display: swap;
+          src: url('/fonts/gothic-a1-v8-latin-regular.eot'); /* IE9 Compat Modes */
+          src: local('Gothic A1 Regular'), local('GothicA1-Regular'),
+               url('/fonts/gothic-a1-v8-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+               url('/fonts/gothic-a1-v8-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-regular.woff') format('woff'), /* Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+               url('/fonts/gothic-a1-v8-latin-regular.svg#GothicA1') format('svg'); /* Legacy iOS */
+        }
+        /* gothic-a1-500 - latin */
+        @font-face {
+          font-family: 'Gothic A1';
+          font-style: normal;
+          font-weight: 500;
+          font-display: swap;
+          src: url('/fonts/gothic-a1-v8-latin-500.eot'); /* IE9 Compat Modes */
+          src: local('Gothic A1 Medium'), local('GothicA1-Medium'),
+               url('/fonts/gothic-a1-v8-latin-500.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+               url('/fonts/gothic-a1-v8-latin-500.woff2') format('woff2'), /* Super Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-500.woff') format('woff'), /* Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-500.ttf') format('truetype'), /* Safari, Android, iOS */
+               url('/fonts/gothic-a1-v8-latin-500.svg#GothicA1') format('svg'); /* Legacy iOS */
+        }
+        /* gothic-a1-600 - latin */
+        @font-face {
+          font-family: 'Gothic A1';
+          font-style: normal;
+          font-weight: 600;
+          font-display: swap;
+          src: url('/fonts/gothic-a1-v8-latin-600.eot'); /* IE9 Compat Modes */
+          src: local('Gothic A1 SemiBold'), local('GothicA1-SemiBold'),
+               url('/fonts/gothic-a1-v8-latin-600.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+               url('/fonts/gothic-a1-v8-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-600.woff') format('woff'), /* Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-600.ttf') format('truetype'), /* Safari, Android, iOS */
+               url('/fonts/gothic-a1-v8-latin-600.svg#GothicA1') format('svg'); /* Legacy iOS */
+        }
+        /* gothic-a1-700 - latin */
+        @font-face {
+          font-family: 'Gothic A1';
+          font-style: normal;
+          font-weight: 700;
+          font-display: swap;
+          src: url('/fonts/gothic-a1-v8-latin-700.eot'); /* IE9 Compat Modes */
+          src: local('Gothic A1 Bold'), local('GothicA1-Bold'),
+               url('/fonts/gothic-a1-v8-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+               url('/fonts/gothic-a1-v8-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-700.woff') format('woff'), /* Modern Browsers */
+               url('/fonts/gothic-a1-v8-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
+               url('/fonts/gothic-a1-v8-latin-700.svg#GothicA1') format('svg'); /* Legacy iOS */
+        }
+        </style>
         <style>
           :root {
             --region-select-width: auto;
