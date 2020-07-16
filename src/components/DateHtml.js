fdd74f66cf32cdc4a73ad15d528b3bbf66531b57
Paul Craig <paul_craig_16@hotmail.com>
2020-07-16 00:36:19 -0400

    trigger ga event on the <time>, not the <a>
    Not sure exactly the issue, but easy to fix once found

--- src/components/DateHtml.js
+++ src/components/DateHtml.js
@@ -1,10 +1,8 @@
 const { displayDate } = require('../dates')
 const { html } = require('../utils')
 
-const DateHtml = ({ dateString, weekday }) => {
-  return html`
-    <time datetime=${dateString}>${displayDate(dateString, weekday)}</time>
-  `
+const DateHtml = ({ dateString, weekday, ...props }) => {
+  return html`<time datetime=${dateString} ...${props}>${displayDate(dateString, weekday)}</time>`
 }
 
 module.exports = DateHtml
