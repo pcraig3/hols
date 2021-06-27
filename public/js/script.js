2b6f4fd59574a544220d161f1b8e82e53dda0fed
Paul Craig <paul@pcraig3.ca>
2021-06-27 01:33:52 -0400

    Add notification banner underneath
    Pretty much the idea here is to be able to communicate updates.
    
    In this specific case, it's helping me with another project.

--- public/js/script.js
+++ public/js/script.js
@@ -35,3 +35,58 @@ for (var i = 0, len = elements.length; i < len; i++) {
     }
   });
 }
+
+//
+
+/*
+ * Inspired by Bootstrap Cookie Alert by Wruczek
+ * https://github.com/Wruczek/Bootstrap-Cookie-Alert
+ */
+(function () {
+  var cookieName = "toast1";
+  var cookieAlert = document.getElementById('toast');
+  var acceptCookies = document.querySelector('.toast--yes');
+
+  if (!cookieAlert) {
+     return;
+  }
+
+  cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
+
+  // Show the alert if we cant find the "toast1" cookie
+  if (!getCookie(cookieName)) {
+      cookieAlert.classList.add("show");
+  }
+
+  // When clicking on the agree button, create a 1 year
+  // cookie to remember user's choice and close the banner
+  acceptCookies.addEventListener("click", function () {
+      setCookie(cookieName, true, 1);
+      cookieAlert.classList.remove("show");
+  });
+
+  // Cookie functions from w3schools
+  function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + "path=/; SameSite=Lax; Secure";
+  }
+
+  function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) === ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) === 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  }
+})();
+
