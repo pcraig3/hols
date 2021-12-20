5182327066f8537d06358b6b8544ebbd92979984
Paul Craig <paul@pcraig3.ca>
2021-12-19 19:03:34 -0500

    Make CalButton way more complicated

--- src/components/CalButton.js
+++ src/components/CalButton.js
@@ -2,21 +2,39 @@ const { html, getProvinceIdOrFederalString } = require('../utils')
 const Button = require('../components/Button.js')
 const { theme } = require('../styles')
 
-const CalButton = ({ federal, provinceId, year = 2021, className = '' }) => {
+const CalButton = ({
+  federal,
+  provinceId,
+  year,
+  text = 'Add to your calendar',
+  className = '',
+  download = false,
+  eventName = 'download-holidays',
+}) => {
   const provinceIdOrFederal = getProvinceIdOrFederalString({ provinceId, federal })
+
+  const getDownloadString = ({ provinceIdOrFederal, year }) => {
+    return `canada-holidays${provinceIdOrFederal ? `-${provinceIdOrFederal}` : ''}${
+      year ? `-${year}` : ''
+    }.ics`
+  }
+
+  const getUrlString = ({ provinceIdOrFederal, year }) => {
+    return `/ics${provinceIdOrFederal ? `/${provinceIdOrFederal}` : ''}${year ? `/${year}` : ''}`
+  }
 
   return html`
     <${Button}
-      href=${`${federal ? '/ics/federal' : provinceId ? `/ics/${provinceId}` : '/ics'}/${year}`}
-      download=${provinceIdOrFederal
-        ? `canada-holidays-${provinceIdOrFederal}-${year}.ics`
-        : `canada-holidays-${year}.ics`}
+      href=${getUrlString({ provinceIdOrFederal, year })}
+      download=${download ? getDownloadString({ provinceIdOrFederal, year }) : null}
       color=${provinceIdOrFederal ? theme.color[provinceIdOrFederal] : {}}
       className=${className}
-      data-event="true"
-      data-action="download-holidays"
-      data-label=${`download-holidays-${provinceIdOrFederal || 'canada'}-${year}`}
-      >Add to your calendar<//
+      data-event=${eventName ? 'true' : null}
+      data-action=${eventName}
+      data-label=${eventName
+        ? `${eventName}-${provinceIdOrFederal || 'canada'}${year ? `-${year}` : ''}`
+        : null}
+      >${text}<//
     >
   `
 }
