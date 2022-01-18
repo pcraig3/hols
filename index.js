044670c88fbb72739978a0adb32c3b1f4767e6ad
Paul Craig <paul@pcraig3.ca>
2022-01-18 01:45:22 -0500

    Remove the old sqlite3 lib, and remove bluebird

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const app = require('./src/server')
-const db = require('sqlite')
-const Promise = require('bluebird')
-
 const DB = require('better-sqlite3-helper')
 
 // // The first call creates the global instance with your settings
@@ -21,18 +18,8 @@ DB({
 // basic HTTP server via express
 const port = parseInt(process.env.PORT, 10) || 3000
 
-Promise.resolve()
-  // First, try to open the database
-  .then(() => db.open('./database.sqlite', { Promise, cached: true })) // <=
-  // Update db schema to the latest version using SQL-based migrations
-  .then(() => db.migrate({ force: 'true' }))
-  // Display error message if something went wrong
-  .catch((err) => console.error(err.stack)) // eslint-disable-line no-console
-  // Finally, launch the Node.js app
-  .finally(() => {
-    return app.listen(port, (err) => {
-      if (err) throw err
-      // eslint-disable-next-line no-console
-      console.log(`Ready on http://localhost:${port}`)
-    })
-  })
+app.listen(port, (err) => {
+  if (err) throw err
+  // eslint-disable-next-line no-console
+  console.log(`Ready on http://localhost:${port}`)
+})
