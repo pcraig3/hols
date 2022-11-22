691796ecc7e38be1c1188a92dd828a919c82f104
Paul Craig <paul@pcraig3.ca>
2022-11-22 14:34:31 -0500

    Revert to original hols backend, remove temporary code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,7 +16,6 @@ const document = ({
       <head>
         ${metaIfSHA() || ''}
         <meta charset="utf-8" />
-        <meta name="keywords" content="test">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${meta ? meta : 'Upcoming statutory holidays in Canada'}">
 
