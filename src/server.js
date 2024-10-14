c1a8b6e1eded0378bd4a1c0f50ca10d13ce68ef2
Paul Craig <paul@pcraig3.ca>
2024-10-14 01:13:30 -0400

    Compression _before_ serving static files

--- src/server.js
+++ src/server.js
@@ -19,8 +19,8 @@ app
   // both of these are needed to parse post request params
   .use(express.urlencoded({ extended: true }))
   .use(express.json())
-  .use(express.static('public', { maxage: process.env.NODE_ENV === 'production' ? '3d' : '0' }))
   .use(compression())
+  .use(express.static('public', { maxage: process.env.NODE_ENV === 'production' ? '3d' : '0' }))
   .use(requestIp.mw())
   .use(cors())
 
