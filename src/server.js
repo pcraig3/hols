8e7adc28d00c1a4f56f1413decaa3719dc891396
Paul Craig <paul@pcraig3.ca>
2024-10-25 01:58:13 -0400

    Cache static files for 90 days instead of 3 days

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app
   .use(express.urlencoded({ extended: true }))
   .use(express.json())
   .use(compression({ brotli: 1, gzip: 1 }))
-  .use(express.static('public', { maxage: process.env.NODE_ENV === 'production' ? '3d' : '0' }))
+  .use(express.static('public', { maxage: process.env.NODE_ENV === 'production' ? '90d' : '0' }))
   .use(requestIp.mw())
   .use(cors())
 
