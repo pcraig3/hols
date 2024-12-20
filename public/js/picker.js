f5eed4ab260e1fd6310291729aa2ad2dcd40b7a3
Paul Craig <paul@pcraig3.ca>
2024-12-20 02:28:16 -0500

    Only run the 'picker' JS if the region select button exists
    Now that this code is included in every page, we need to be more
    careful about checking these things.

--- public/js/picker.js
+++ public/js/picker.js
@@ -2,23 +2,25 @@
 var selectPadding = 42;
 var root = document.documentElement;
 var regionButton = document.getElementById('region-select__button');
-regionButton.setAttribute('data-display-none', true);
+if (regionButton) {
+  regionButton.setAttribute('data-display-none', true);
 
-var pickerContainer = document.getElementById('picker-container');
-pickerContainer.setAttribute('data-hidden', false);
+  var pickerContainer = document.getElementById('picker-container');
+  pickerContainer.setAttribute('data-hidden', false);
 
-function addChangeListener(node) {
-  node.addEventListener('change', function (event) {
-    // node[node.selectedIndex].value
-    event.target.form.submit();
-  });
+  function addChangeListener(node) {
+    node.addEventListener('change', function (event) {
+      // node[node.selectedIndex].value
+      event.target.form.submit();
+    });
+  }
+
+  var regionSelect = document.getElementById('region-select');
+  addChangeListener(regionSelect);
+
+  var yearSelect = document.getElementById('year-select');
+  addChangeListener(yearSelect);
+
+  var regionSelectWidth = document.getElementById('region-select-width');
+  root.style.setProperty('--region-select-width', regionSelectWidth.offsetWidth + selectPadding + "px");
 }
-
-var regionSelect = document.getElementById('region-select');
-addChangeListener(regionSelect);
-
-var yearSelect = document.getElementById('year-select');
-addChangeListener(yearSelect);
-
-var regionSelectWidth = document.getElementById('region-select-width');
-root.style.setProperty('--region-select-width', regionSelectWidth.offsetWidth + selectPadding + "px");
