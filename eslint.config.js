a6a8b38c89212f78212b2cb2c36f2b76cf7cbad1
Paul Craig <paul@pcraig3.ca>
2026-06-30 00:47:52 -0400

    Upgrade ESLint to version 10
    I changed a few things in the configuration file, and overall
    it looks like things are fine.

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,23 @@
+const { defineConfig } = require('eslint/config')
 const globals = require('globals')
 
-module.exports = {
-  languageOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-    globals: { ...globals.node },
-  },
-  plugins: {
-    jest: {
-      globals: true,
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1, ignoredNodes: ['TemplateLiteral *'] }],
+      'linebreak-style': 'off',
+      quotes: ['error', 'single'],
+      'comma-dangle': ['error', 'always-multiline'],
+      semi: ['error', 'never'],
+      'no-console': 'error',
     },
   },
-  rules: {
-    indent: ['error', 2, { SwitchCase: 1, ignoredNodes: ['TemplateLiteral *'] }],
-    'linebreak-style': 'off', // Changed from 0 to 'off' to use string values
-    quotes: ['error', 'single'],
-    'comma-dangle': ['error', 'always-multiline'],
-    semi: ['error', 'never'],
-    'no-console': 'error',
-  },
-}
+])
